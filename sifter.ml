001fce53508cc1e3e95553f21a8b6a875aa77bdb
Gabriel <g2p.code@gmail.com>
2010-01-28 01:04:36 +0100

    IO starting to work.
    The inode model seems to be simpler for us though.
    
    Will switching kill some code?

--- sifter.ml
+++ sifter.ml
@@ -8,45 +8,132 @@ open Printf
 open Git
 open Git_types
 
+
+
 let default_stats = LargeFile.stat "."
 let fname = "hello"
 let name = "/" ^ fname
-let contents : Fuse.buffer = Array1.of_array Bigarray.char Bigarray.c_layout 
+let contents : Fuse.buffer = Array1.of_array Bigarray.char Bigarray.c_layout
   [|'H';'e';'l';'l';'o';' ';'w';'o';'r';'l';'d';'!'|]
 
-let do_getattr path = 
+
+type hash = string
+
+type scaff_assoc = (string * scaffolding) list
+and scaff_assoc_io = unit -> scaff_assoc
+and scaffolding =
+  |DirectScaff of scaff_assoc
+  |IOScaff of scaff_assoc_io
+  |MixedScaff of scaff_assoc_io * (string -> scaffolding)
+  |FileHash of hash
+  |TreeHash of hash
+  |CommitHash of hash
+  |OtherHash of hash (* gitlink, etc *)
+
+
+let scaffolding_child scaff child =
+  match scaff with
+  |DirectScaff li -> List.assoc child li
+  |IOScaff f -> List.assoc child (f ())
+  |MixedScaff (_, lookup) -> lookup child
+  |FileHash _ -> raise Not_found
+  |TreeHash _ -> raise Not_found (* XXX *)
+  |CommitHash _ -> raise Not_found (* XXX *)
+  |OtherHash _ -> raise Not_found
+
+
+let list_children = function
+  |DirectScaff li -> li
+  |IOScaff f -> f ()
+  |MixedScaff (f, _) -> f ()
+  |FileHash _ -> raise Not_found
+  |TreeHash _ -> raise Not_found (* XXX *)
+  |CommitHash _ -> raise Not_found (* XXX *)
+  |OtherHash _ -> raise Not_found
+
+let lookup scaff path =
+  let rec lookup_r scaff = function
+    |[] -> scaff
+    |dir::rest ->
+	lookup_r (scaffolding_child scaff dir) rest
+  in match BatString.nsplit path "/" with
+  |""::""::[] -> lookup_r scaff []
+  |""::path_comps ->
+      lookup_r scaff path_comps
+  |_ -> assert false (* fuse path must start with a slash *)
+
+
+let trees = MixedScaff ((fun () -> []), fun _ -> raise Not_found)
+let root = DirectScaff [("trees", trees)]
+
+
+let fh_data = Hashtbl.create 16
+let fh_by_name = Hashtbl.create 16
+
+let next_fh = ref 0
+
+let lookup_and_cache path =
+  try
+    Hashtbl.find fh_by_name path
+  with Not_found ->
+    let scaff = lookup root path in
+    let fh = !next_fh in
+    incr next_fh;
+    Hashtbl.add fh_by_name path (fh, scaff);
+    Hashtbl.add fh_data fh scaff;
+    (fh, scaff)
+
+let lookup_fh fh =
+  Hashtbl.find fh_data fh
+
+
+
+let do_getattr path =
   if path = "/" then default_stats
-  else if path = name then 
-    { default_stats with 
+  else if path = name then
+    { default_stats with
       st_nlink = 1;
       st_kind = S_REG;
       st_perm = 0o444;
       st_size = Int64.of_int (Array1.dim contents) }
   else raise (Unix_error (ENOENT,"stat",path))
 
-let do_readdir path _ =
-  if path = "/" then [".";"..";fname]
-  else raise (Unix_error (ENOENT,"readdir",path))
+let do_opendir path flags =
+  prerr_endline ("Path is: " ^ path);
+  try
+    let fh, scaff = lookup_and_cache path in
+    Some fh
+  with Not_found ->
+    raise (Unix_error (ENOENT,"opendir",path))
+
+let do_readdir path fh =
+  try
+    let scaff = lookup_fh fh in
+    "."::".."::(List.map fst (list_children scaff))
+  with Not_found ->
+    assert false (* because opendir passed *)
+    (*raise (Unix_error (ENOENT,"readdir",path))*)
 
 let do_fopen path flags =
   if path = name then None
   else raise (Unix_error (ENOENT,"open",path))
 
-let do_read path buf ofs _ =
+let do_read path buf ofs fh =
   if path = name then
-    if ofs > (Int64.of_int max_int) then 0    
+    if ofs > (Int64.of_int max_int) then 0
     else
       let ofs = Int64.to_int ofs in
       let len = min ((Array1.dim contents) - ofs) (Array1.dim buf) in
 	(Array1.blit (Array1.sub contents ofs len) (Array1.sub buf 0 len);
 	 len)
   else raise (Unix_error (ENOENT,"read",path))
-	   
+
 let _ =
-  main Sys.argv 
-    { 
-      default_operations with 
+  main Sys.argv
+    {
+      default_operations with
 	getattr = do_getattr;
+	opendir = do_opendir;
 	readdir = do_readdir;
 	fopen = do_fopen;
 	read = do_read;
