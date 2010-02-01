35d6e502f9a87252b6793a6f029d732d3f7e9d0a
Gabriel <g2p.code@gmail.com>
2010-02-01 01:33:39 +0100

    Refs symlink to commits.

--- sifter.ml
+++ sifter.ml
@@ -25,10 +25,10 @@ let backtick shell_cmd =
   BatIO.read_all out_pipe
 
 let trim_endline str =
-  (* XXX not what the spec says *)
+  (* XXX not what the spec says, this trims both ends *)
   BatString.trim str
 
-let git_wt = "/home/g2p/var/co/git/ocaml-git"
+let git_wt = "/home/g2p/var/co/git/ocaml-git" (* XXX *)
 let git_dir = git_wt ^ "/.git"
 
 let repo = wait_on_monad (Git.Repo.repo git_wt)
@@ -45,6 +45,9 @@ let file_stats = { dir_stats with
   (* /proc uses zero, it works. /sys uses 4k. *)
   st_size = Int64.zero;
   }
+let symlink_stats = { file_stats with
+  st_kind = S_LNK;
+  }
 
 let fname = "hello"
 let name = "/" ^ fname
@@ -60,9 +63,12 @@ and scaffolding =
   |RootScaff
   |TreesScaff
   |RefsScaff
+  |RefScaff of string
+  |Symlink of string
   |FileHash of hash
   |TreeHash of hash
   |CommitHash of hash
+  |CommitMsg of hash
   |OtherHash of hash (* gitlink, etc *)
 
 
@@ -70,6 +76,7 @@ and scaffolding =
 let root_al = [
   "trees", TreesScaff;
   "refs", RefsScaff;
+  (*"commits", CommitsScaff;*)
   ]
 
 let slash_free s =
@@ -92,34 +99,75 @@ let commit_of_ref ref =
   *)
   trim_endline (backtick_git [ "show-ref"; "--hash"; "--verify"; "--"; ref ])
 
+let tree_of_commit hash =
+  trim_endline (backtick_git [ "rev-parse"; "--revs-only"; "--no-flags";
+  "--verify"; "--quiet"; hash ^ "^{tree}" ])
+
+let rec parents_depth depth =
+  if depth = 0 then ""
+  else "../" ^ parents_depth (depth - 1)
+
+let commit_symlink_of_ref ref depth =
+  let to_root = parents_depth depth in
+  Symlink (to_root ^ "commits/" ^ (commit_of_ref ref))
+
+let tree_symlink_of_commit hash depth =
+  let to_root = parents_depth depth in
+  Symlink (to_root ^ "trees/" ^ (tree_of_commit hash))
+
+let depth_of_scaff = function
+  |RootScaff -> 0
+  |RefScaff _ -> 2
+  |CommitHash _ -> 2
+  |_ -> failwith "not implemented"
+
 let scaffolding_child scaff child =
   match scaff with
   |RootScaff -> List.assoc child root_al
   |TreesScaff -> raise Not_found (* XXX *)
-  |RefsScaff -> let ref = reslash child in CommitHash (commit_of_ref ref)
+  |RefsScaff -> let ref = reslash child in RefScaff ref
   |FileHash _ -> raise Not_found
   |TreeHash _ -> raise Not_found (* XXX *)
-  |CommitHash _ -> raise Not_found (* XXX *)
+  |RefScaff name ->
+    if child = "current" then commit_symlink_of_ref name (depth_of_scaff scaff)
+    (*else if child = "reflog" then ReflogScaff name*)
+    else raise Not_found
+  |CommitHash hash ->
+    if child = "msg" then CommitMsg hash
+    else if child = "worktree" then tree_symlink_of_commit hash (depth_of_scaff
+    scaff)
+    (*else if child = "parents" then CommitParents hash*)
+    else raise Not_found
+  |CommitMsg _ -> raise Not_found
+  |Symlink _ -> raise Not_found
   |OtherHash _ -> raise Not_found
 
 let list_children = function
-  |RootScaff -> root_al
+  |RootScaff -> List.map fst root_al
   |TreesScaff -> [] (* XXX *)
   |RefsScaff -> let heads = wait_on_monad (repo#heads ()) in
-  List.map (fun (n, h) -> slash_free (trim_endline n), CommitHash (Git_types.string_of_id h)) heads
+   List.map (fun (n, h) -> slash_free (trim_endline n)) heads
+  |RefScaff name -> [ "current"; (*"reflog";*) ]
   |TreeHash _ -> [] (* XXX *)
   |CommitHash _ -> [] (* XXX *)
   |FileHash _ -> raise Not_found
   |OtherHash _ -> raise Not_found
+  |CommitMsg _ -> raise Not_found
+  |Symlink _ -> raise Not_found
 
 let is_container = function
   |RootScaff -> true
   |TreesScaff -> true
   |RefsScaff -> true
-  |FileHash _ -> false
   |TreeHash _ -> true
   |CommitHash _ -> true
+  |RefScaff _ -> true
+
+  |FileHash _ -> false
   |OtherHash _ -> false
+  |CommitMsg _ -> false
+  |Symlink _ -> false
+
 
 let lookup scaff path =
   let rec lookup_r scaff = function
@@ -157,11 +205,22 @@ let lookup_fh fh =
 
 let do_getattr path =
   try
-    let scaff = lookup RootScaff path in
-    if is_container scaff
+   let scaff = lookup RootScaff path in
+   (*if is_container scaff
     then dir_stats
-    else file_stats
-  with Not_found ->
+    else file_stats*)
+   match scaff with
+   |RootScaff -> dir_stats
+   |TreesScaff -> dir_stats
+   |RefsScaff -> dir_stats
+   |TreeHash _ -> dir_stats
+   |CommitHash _ -> dir_stats
+   |RefScaff _ -> dir_stats
+   |FileHash _ -> file_stats
+   |OtherHash _ -> file_stats
+   |CommitMsg _ -> file_stats
+   |Symlink _ -> symlink_stats
+   with Not_found ->
     raise (Unix_error (ENOENT, "stat", path))
 
 let do_opendir path flags =
@@ -175,10 +234,20 @@ let do_opendir path flags =
 let do_readdir path fh =
   try
     let scaff = lookup_fh fh in
-    "."::".."::(List.map fst (list_children scaff))
+    "."::".."::(list_children scaff)
   with Not_found ->
     assert false (* because opendir passed *)
     (*raise (Unix_error (ENOENT, "readdir", path))*)
+
+let do_readlink path =
+  try
+   let scaff = lookup RootScaff path in
+   match scaff with
+   |Symlink target -> target
+   |_ -> raise (Unix_error (EINVAL, "readlink (not a link)", path))
+   with Not_found ->
+    raise (Unix_error (ENOENT, "readlink", path))
+
 
 let do_fopen path flags =
   if path = name then None
@@ -201,6 +270,7 @@ let _ =
 	getattr = do_getattr;
 	opendir = do_opendir;
 	readdir = do_readdir;
+	readlink = do_readlink;
 	fopen = do_fopen;
 	read = do_read;
     }
